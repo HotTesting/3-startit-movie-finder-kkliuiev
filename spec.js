52953497a23251011f12ba2b3a973a181ef32603
Kostiantyn Kliuiev <kliuiev1981@gmail.com>
2017-02-21 21:03:30 +0200

    First Test

--- spec.js
+++ spec.js
@@ -1,5 +1,15 @@
-describe('', ()=> {
-    it('', ()=> {
-        expect(true).toBe(true)
+describe('Test Suite', () => {
+    it('Test Case', () => {
+        browser.get('')
+        let searchField = $("input[name='searchStr']")
+        let searchRequest = 'The Lord'
+        searchField.sendKeys(searchRequest)
+        element(by.buttonText('Go!')).click()
+        browser.sleep(5000)
+        let movieCard = $('movie-card')
+        let title = movieCard.$('h4 a').getText()
+        expect(title).toContain(searchRequest, 'First search result should contain search string')
+
     })
+
 })
