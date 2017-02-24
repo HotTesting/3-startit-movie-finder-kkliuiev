22ae8f3045052d2fd9f8e5fd6b8dcd8fd61b463b
Kostiantyn Kliuiev <kliuiev1981@gmail.com>
2017-02-24 18:00:30 +0200

    Small changes

--- spec1.js
+++ spec1.js
@@ -1,13 +1,15 @@
 describe('Test Suite', () => {
     it('Test Case01', () => {
-        browser.get('')
-        let searchField = $("input[name='searchStr']")
+        browser.get('');
+        let searchField = $("input[name='searchStr']");
+        let searchRequest = 'Matrix';
         searchField.sendKeys(searchRequest);
-        element(by.buttonText('Go!')).click()
-        browser.sleep(5000)
-        let movieCard = $('movie-card')
-        let title = movieCard.$('h4 a').getText()
-        expect(title).toContain(searchRequest, 'First search result should contain search string')
+        element(by.buttonText('Go!')).click();
+        browser.sleep(5000);
+        let allArray = $$('h3 + div').get(0);
+        allArray.$$("h4 a").getText().map(text => {
+            expect(text).toContain('Matrix')
+        })
 
     })
 
