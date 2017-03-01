c8193180c728f80f53559fa5db5bbceb116551e9
Kostiantyn Kliuiev <kliuiev1981@gmail.com>
2017-03-01 12:05:08 +0200

    finally

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,7 @@ module.exports.config = {
     specs: ['spec.js'],
     directConnect: true,
     baseUrl: 'https://movies-finder.firebaseapp.com/',
-    beforeAll:() =>  {
+    onPrepare:() =>  {
         
        let reporter = require('jasmine2-reporter')
        jasmine.getEnv().addReporter(reporter);
