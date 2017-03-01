1215b5ccef575a08e0ff713505d1a266ff3bcb2f
Kostiantyn Kliuiev <kliuiev1981@gmail.com>
2017-03-01 17:21:48 +0200

    Dataprovider used for list search

--- spec2.js
+++ spec2.js
@@ -1,18 +1,17 @@
 describe('Test Suite', () => {
-    it('Test Case01', () => {
-        browser.get('');
-        let searchField = $("input[name='searchStr']");
-        let searchRequest = 'Matrix';
-        searchField.sendKeys(searchRequest);
-        element(by.buttonText('Go!')).click();
-        browser.sleep(5000);
-        let allArray = $$('h3 + div').get(0);
-        let allText = allArray.$$("h4 a").getText().then(texts=> {
-            texts.map(text=> {
-                expect(text).toContain('Matrix','Search result contains wrong strings')
-            })
-       
-    });
+      let dataCollection = ["Action","Adventure","Animation","Comedy","Crime","Documentary","Drama","Family","Fantasy","History","Horror","Music","Mystery","Romance","Science Fiction","TV Movie","Thriller","War","Western"]
+    dataCollection.map(data=> {
+        
+        it(`should redirect to items with relevant badges for ${data}`, () => {
+            // Text for correct Genre badge on Movie page
+            browser.get('')
+            element(by.linkText(data)).click();
+            browser.sleep(5000);
+            $('h4 a').click();
+            browser.sleep(5000);
+            let smallButtons = element.all(by.className('label label-info m-r-md')).getText();
+            expect(smallButtons).toContain(data, 'Wrong page is shown!!')
+        })
 
-})
+    })
 })
