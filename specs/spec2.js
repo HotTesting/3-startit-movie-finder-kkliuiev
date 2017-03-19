3636a6f668438b23bf04356ad3f3a67a8641daaf
Kostiantyn Kliuiev <kliuiev@icloud.com>
2017-03-19 17:47:51 +0200

    Trying add cycle

--- specs/spec2.js
+++ specs/spec2.js
@@ -1,35 +1,36 @@
 describe('Kasa.in.ua', () => { 
     browser.ignoreSynchronization = true
      it('should allow to sell tickets', () => {
-
+        for (let i = 0; i< 10; i++) {
         browser.get('')
         let getMoreButtonWait = protractor.ExpectedConditions.presenceOf($('#getMoreEvents'))
-        browser.wait(getMoreButtonWait, 5000, 'First search result is not visible')
+        browser.wait(getMoreButtonWait, 9000, 'First search result is not visible')
         $('#getMoreEvents').click();
         browser.manage().timeouts().implicitlyWait(9000);
         $('a[href*="payment-and-delivery"]').click();
         $('a[href*="#event-map-anchor"]').click();
-        browser.sleep(2000)
+        browser.sleep(1000)
         $('#row0col0sector2436').click();
-         browser.sleep(2000)
+         browser.sleep(1000)
         $('a[href*="/create-order"]').click();
-         browser.sleep(2000)
+         browser.sleep(3000)
         $('#tickets-submit').click();
-         browser.sleep(2000)
+         browser.sleep(3000)
         $('a[href*="#payment-1"]').click();
-         browser.sleep(2000)
+         browser.sleep(3000)
         element(by.buttonText('Оплата готівкою в касі')).click()
          browser.sleep(2000)
-         $('#pickupcheckoutform-firstname').sendKeys('Test Name')
-         $('#pickupcheckoutform-lastname').sendKeys('Test LastName')
+         $('#pickupcheckoutform-firstname').sendKeys('Test Name' + i)
+         $('#pickupcheckoutform-lastname').sendKeys('Test LastName' + i )
          $('#pickupcheckoutform-phone').sendKeys('0123456789')
          $('#pickupcheckoutform-email').sendKeys('test@test.com')
-         $('#pickupcheckoutform-usermessage').sendKeys('Test step ')
-         
-        let Target = $$('.row5').$$('article').$$('.inner').$$('.title').getText().then(texts=>texts.map(text => {console.log(text)}));
-        //let Click = $('btn-buy').click();
-        expect(true).toBe(true)
+         $('#pickupcheckoutform-usermessage').sendKeys('Test step' + i)
+         $('#pickupcheckoutform-acceptorder').click();
+         browser.sleep(3000)
+         element(by.buttonText('Підтвердити та оформити замовлення')).click()
+
+        expect($('.col-sm-12').$$('.text-bold').get(0).getText()).toContain('Вітаємо!')}
      }
-     ),40000
+     ,50000)
 }
 )
